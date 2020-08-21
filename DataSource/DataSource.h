50084be088a9b3c40d89ff429a4f43b92961a0e6
ichikawa7ss <i2s7.chacha@gmail.com>
2020-08-21 20:29:14 +0900

    Add DataSource and Domain

---
+++ DataSource/DataSource.h
@@ -0,0 +1,18 @@
+//
+//  DataSource.h
+//  DataSource
+//
+//  Created by ichikawa on 2020/08/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DataSource.
+FOUNDATION_EXPORT double DataSourceVersionNumber;
+
+//! Project version string for DataSource.
+FOUNDATION_EXPORT const unsigned char DataSourceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DataSource/PublicHeader.h>
+
+
