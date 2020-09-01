50ae557bb562f4b8645c1dc3f08350c8b50a690b
ichikawa7ss <i2s7.chacha@gmail.com>
2020-09-01 20:01:24 +0900

    Remove default file and storyboard

--- smart-novel-ios/AppDelegate.swift
+++ smart-novel-ios/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Presentation
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -13,31 +14,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        if #available(iOS 13.0, *) {
-        } else {
-            self.window = UIWindow(frame: UIScreen.main.bounds)
-            let storyboard = UIStoryboard(name: "Main", bundle: nil)
-            let initialViewController = storyboard.instantiateViewController(withIdentifier: "initial")
-            self.window?.rootViewController = initialViewController
-            self.window?.makeKeyAndVisible()
-        }
+
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        let navigationController = SmartNovelNavigationController(rootViewController: NovelListBuilder.build())
+        self.window?.rootViewController = navigationController
+        self.window?.makeKeyAndVisible()
+
         return true
-    }
-
-    // MARK: UISceneSession Lifecycle
-
-    @available(iOS 13.0, *)
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    @available(iOS 13.0, *)
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 }
 
