a73b455bb1d47252842782af354239fa09d4c051
ichikawa7ss <i2s7.chacha@gmail.com>
2020-09-05 07:42:03 +0900

    Add request base protocol

---
+++ DataStore/Entity/API/Request/Base/APIRequestable.swift
@@ -0,0 +1,11 @@
+//
+//  APIRequestable.swift.swift
+//  DataStore
+//
+//  Created by ichikawa on 2020/09/05.
+//
+
+import Foundation
+import APIKit
+
+protocol APIRequestable: Request {}
---
+++ DataStore/Entity/API/Request/Type/NovelListAPIRequest.swift
@@ -0,0 +1,9 @@
+//
+//  NovelListAPIRequest.swift
+//  DataStore
+//
+//  Created by ichikawa on 2020/09/05.
+//
+
+import Foundation
+
