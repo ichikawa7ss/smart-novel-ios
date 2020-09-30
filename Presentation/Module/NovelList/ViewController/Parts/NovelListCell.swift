cd80854845a1da43ce02c9e2fb0aba37b224d8b5
ichikawa7ss <i2s7.chacha@gmail.com>
2020-09-30 21:25:30 +0900

    Impl base NovelListCell.

--- Presentation/Module/NovelList/ViewController/Parts/NovelListCell.swift
+++ Presentation/Module/NovelList/ViewController/Parts/NovelListCell.swift
@@ -16,7 +16,22 @@ final class NovelListCell: UITableViewCell {
     
     @IBOutlet private weak var dateLabel: UILabel!
     
+    @IBOutlet private weak var siteColorView: UIView!
+    
+    @IBOutlet private weak var tagLabel: UILabel!
+    
     func setData(_ item: NovelListModel.Novel) {
         // TODO : NovelList用に差し替え
+        
+        /// TODO: タグの作成
+        /// タグはラベルの文字列をUnicode変換？→色を生成
+        /// 色の精製方法は以下の5番を使えばいいかな
+        /// https://q-az.net/random-color-code/
+        ///
+        
+        self.novelTitleLabel.text = item.title
+        self.authorLabel.text = item.author
+        self.dateLabel.text = item.updateTime
+        self.tagLabel.text = item.tags.map { $0.name }.joined(separator: " ")
     }
 }
