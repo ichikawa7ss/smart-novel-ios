8ebc502a984d33b0f318f9e7b537d952ce92349f
ichikawa7ss <i2s7.chacha@gmail.com>
2020-10-27 23:44:48 +0900

    Set individual genre icon.

--- Presentation/Module/Search/ViewController/Parts/GenreGroupTableCell/GenreItemView.swift
+++ Presentation/Module/Search/ViewController/Parts/GenreGroupTableCell/GenreItemView.swift
@@ -20,11 +20,31 @@ final class GenreItemView: BaseView {
     
     func setData(_ genre: NovelListModel.Novel.SearchableGenre) {
         self.label.text = genre.rawValue
-        self.icon.image = UIImage(named: "robot")
+        self.icon.image = genre.generateIcon()
         
         self.itemButton.didTouchUpInsideObservable
             .map { genre }
             .bind(to: self.itemButtonDidTapRelay)
             .disposed(by: self.disposeBag)
+    }
+}
+
+extension NovelListModel.Novel.SearchableGenre {
+    
+    func generateIcon() -> UIImage? {
+        switch self {
+        case .fantasy:
+            return UIImage(named: "fantasy")
+        case .love:
+            return UIImage(named: "heart")
+        case .mystery:
+            return UIImage(named: "mystery")
+        case .drama:
+            return UIImage(named: "drama")
+        case .essay:
+            return UIImage(named: "essay")
+        case .sf:
+            return UIImage(named: "robot")
+        }
     }
 }
