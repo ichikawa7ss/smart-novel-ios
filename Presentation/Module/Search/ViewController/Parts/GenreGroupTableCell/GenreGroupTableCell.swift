184824ba5fa75aa7024c5548fe0d66da00146c8b
ichikawa7ss <i2s7.chacha@gmail.com>
2020-10-27 23:00:21 +0900

    Impl GenreCell.

--- Presentation/Module/Search/ViewController/Parts/GenreGroupTableCell/GenreGroupTableCell.swift
+++ Presentation/Module/Search/ViewController/Parts/GenreGroupTableCell/GenreGroupTableCell.swift
@@ -5,4 +5,29 @@
 //  Created by ichikawa on 2020/10/27.
 //
 
-import Foundation
+import UIKit
+import RxSwift
+import RxCocoa
+import Domain
+
+final class GenreGroupTableCell: UITableViewCell {
+
+    @IBOutlet private var genreItemViewCollection: [GenreItemView]!
+    
+    var genreViewDidTapRelay = PublishRelay<NovelListModel.Novel.Tag>()
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    func setData(_ tags: [NovelListModel.Novel.Tag]) {
+        self.genreItemViewCollection.enumerated().map {
+            $0.element.setData(tags[$0.offset])
+            $0.element.itemButtonDidTapRelay
+                .bind (onNext:{ tag in
+                    self.genreViewDidTapRelay.accept(tag)
+                })
+                .disposed(by: self.disposeBag)
+        }
+    }
+}
--- Presentation/Module/Search/ViewController/Parts/GenreGroupTableCell/GenreItemView.swift
+++ Presentation/Module/Search/ViewController/Parts/GenreGroupTableCell/GenreItemView.swift
@@ -5,4 +5,26 @@
 //  Created by ichikawa on 2020/10/27.
 //
 
-import Foundation
+import Domain
+import UIKit
+import RxSwift
+import RxCocoa
+
+final class GenreItemView: BaseView {
+
+    @IBOutlet private weak var label: UILabel!
+    @IBOutlet private weak var icon: UIImageView!
+    @IBOutlet private weak var itemButton: HoverView!
+    
+    var itemButtonDidTapRelay = PublishRelay<NovelListModel.Novel.Tag>()
+    
+    func setData(_ tag: NovelListModel.Novel.Tag) {
+        self.label.text = tag.name
+        self.icon.image = UIImage(named: "robot")
+        
+        self.itemButton.didTouchUpInsideObservable
+            .map { tag }
+            .bind(to: self.itemButtonDidTapRelay)
+            .disposed(by: self.disposeBag)
+    }
+}
